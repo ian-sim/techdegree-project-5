c628238aafcbb6fe637580d1a574422b89e19b2f
ian-sim <i.simpson8891@gmail.com>
2020-08-24 14:13:28 +0800

    completed exceeds requirements

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -10,9 +10,71 @@ function fetchData(url) {
 			console.log("Oops, looks like there was a problem!", error)
 		);
 }
+
 insertSearch();
 // Request profile data and generate cards
 fetchData(usersURL).then((data) => generateProfiles(data.results));
+
+// function to create modal window
+function createModal(data, employeeInfo) {
+	gallery.insertAdjacentHTML(
+		"afterend",
+		`<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${
+							employeeInfo.picture.medium
+						}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employeeInfo.name.first} ${
+			employeeInfo.name.last
+		}</h3>
+            <p class="modal-text">${employeeInfo.email}</p>
+            <p class="modal-text cap">${employeeInfo.location.city}</p>
+            <hr>
+            <p class="modal-text">${formatPhoneNumber(employeeInfo.cell)}</p>
+            <p class="modal-text">${employeeInfo.location.street.number} ${
+			employeeInfo.location.street.name
+		}, ${employeeInfo.location.city}, ${employeeInfo.location.state} ${
+			employeeInfo.location.postcode
+		}</p>
+            <p class="modal-text">Birthday: ${formatBirthday(
+							employeeInfo.dob.date
+						)}</p>
+        </div>
+    </div>
+    <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+</div>`
+	);
+	// Select close button and add listener
+	const closeBtn = document.querySelector("button.modal-close-btn");
+	const modalContainer = document.querySelector("div.modal-container");
+
+	closeBtn.addEventListener("click", () => {
+		modalContainer.remove();
+	});
+
+	// Select prev and next buttons and add functionality
+	let index = data.indexOf(employeeInfo);
+	const nextButton = document.getElementById("modal-next");
+	const prevButton = document.getElementById("modal-prev");
+
+	nextButton.addEventListener("click", () => {
+		if (index + 1 < 12) {
+			modalContainer.remove();
+			createModal(data, data[index + 1]);
+		}
+	});
+	prevButton.addEventListener("click", () => {
+		if (index > 0) {
+			modalContainer.remove();
+			createModal(data, data[index - 1]);
+		}
+	});
+}
 
 // Function to generate profiles with data from fetch request
 function generateProfiles(data) {
@@ -39,44 +101,8 @@ function generateProfiles(data) {
 	for (let i = 0; i < profileCards.length; i++) {
 		profileCards[i].addEventListener("click", () => {
 			if (modal.length === 0) {
-				gallery.insertAdjacentHTML(
-					"afterend",
-					`<div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="${
-													data[i].picture.medium
-												}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${
-													data[i].name.first
-												} ${data[i].name.last}</h3>
-                        <p class="modal-text">${data[i].email}</p>
-                        <p class="modal-text cap">${data[i].location.city}</p>
-                        <hr>
-                        <p class="modal-text">${formatPhoneNumber(
-													data[i].cell
-												)}</p>
-                        <p class="modal-text">${
-													data[i].location.street.number
-												} ${data[i].location.street.name}, ${
-						data[i].location.city
-					}, ${data[i].location.state} ${data[i].location.postcode}</p>
-                        <p class="modal-text">Birthday: ${formatBirthday(
-													data[i].dob.date
-												)}</p>
-                    </div>
-                </div>`
-				);
+				createModal(data, data[i]);
 			}
-			// Select close button and add listener
-			const closeBtn = document.querySelector("button.modal-close-btn");
-			const modalContainer = document.querySelector("div.modal-container");
-			const parent = modalContainer.parentNode;
-
-			closeBtn.addEventListener("click", () => {
-				parent.removeChild(modalContainer);
-			});
 		});
 	}
 }
@@ -86,6 +112,8 @@ function checkStatus(response) {
 	if (response.ok) {
 		return Promise.resolve(response);
 	} else {
+		gallery.innerHTML = "<h1>Oops, it looks like there was a problem!</h1>";
+		gallery.style.color = "red";
 		return Promise.reject(new Error(response.statusText));
 	}
 }
@@ -115,6 +143,8 @@ function formatBirthday(birthday) {
 	return formattedBirthday.replace(/(\d{4})(\d{2})(\d{2})/, "$2/$3/$1");
 }
 
+// THE BELOW CODE IS FOR THE EXCEEDS EXPECTATIONS REQUIREMENTS :)
+
 // Create function to insert search bar
 function insertSearch() {
 	const searchContainer = document.querySelector("div.search-container");
@@ -123,3 +153,41 @@ function insertSearch() {
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`;
 }
+
+// Create function to compare input in search to employees and display any results found with a match
+const searchInput = document.getElementById("search-input");
+const searchButton = document.querySelector("#search-submit");
+function searchName(searchInput) {
+	const users = document.querySelectorAll("div.card");
+	if (searchInput.value.length === 0) {
+		for (let j = 0; j < users.length; j++) {
+			users[j].style.display = "";
+		}
+	} else {
+		for (let i = 0; i < users.length; i++) {
+			users[i].style.display = "none";
+			const name = users[
+				i
+			].lastElementChild.firstElementChild.textContent.toLowerCase();
+			const input = searchInput.value.toLowerCase();
+			if (input.length !== 0 && name.includes(input)) {
+				users[i].style.display = "";
+			}
+		}
+	}
+}
+
+// Event listener for search Button to search and display results, if any
+searchButton.addEventListener("click", () => {
+	searchName(searchInput);
+});
+
+// Event listener for keyup in input field to search and display results, if any
+searchInput.addEventListener("keyup", () => {
+	searchName(searchInput);
+});
+
+// Event listener for click in input field to search and display results, if any
+searchInput.addEventListener("click", () => {
+	searchName(searchInput);
+});
